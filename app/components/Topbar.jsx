2a47e852cb7682e67d04786d84a4b4f4c5f63246
Prince Kumar <pk7411956419@gmail.com>
2024-09-04 22:18:55 +0530

    Added routes and fixed image

--- app/components/Topbar.jsx
+++ app/components/Topbar.jsx
@@ -1,17 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 function Topbar() {
   return (
     <div className="topbar flex justify-between bg-green-700 font-semibold px-3 py-2 rounded-md text-white">
-        <button className="home">
-            Home
-        </button>
-        <button className="ctrl">
-            Control Panel
-        </button>
-        <button className="login">
-            Login Now
-        </button>
+        <Link href='/'>Home</Link>
+        <Link href="/control">Control Panel</Link>
+        <Link href="/login">Login</Link>
     </div>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,13 @@
-import Topbar from "./components/Topbar";
 import Image from "next/image";
 
 export default function () {
   return( 
   <>
     <div className="main min-h-screen min-w-full">
-      <Topbar/>
-      <div className="hero-image border-2 border-black h-[35rem] rounded-r-md">
-        <Image src={'/image.png'} className="object-cover w-screen h-[34rem] pt-2" width={1000} height={1000}/>
+      <div className="hero-image h-[35rem] rounded-r-md mt-2">
+        <Image src={'/image.png'} className="object-fill w-full h-full rounded-md " width={1000} height={1000}/>
       </div>
+      <hr className="mt-5"/>
       <div className="graphs grid grid-cols-2 w-screen px-5 mt-5 gap-3 align-center h-52 border-black border-2">
         <div className="AQI border-2 border-purple-600 text-center w-1/2">aqi</div>
         <div className="ratio border-2 border-purple-600 text-center w-1/2">ratio</div>
