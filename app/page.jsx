a1e1ee2d50d28a3345ff150a1359ce734278ef2e
Prince Kumar <pk7411956419@gmail.com>
2024-09-06 15:47:23 +0530

    new commit

--- app/page.jsx
+++ app/page.jsx
@@ -1,31 +1,38 @@
 import Image from "next/image";
 
+
 export default function () {
   return( 
   <>
     <div className="main min-h-screen min-w-full">
       <div className="hero-image h-[35rem] flex relative rounded-md">
         <Image src={'/image1.png'} className="rounded-md opacity-70 object-cover w-full h-full" width={1000} height={1500}/>
-        <div className="absolute font-extrabold ml-[33rem] my-[13rem] text-white text-6xl">
-          <h1 className="align-center">Algae  Based</h1>
+        <div className="absolute w-screen text-center font-extrabold my-[13rem] text-white text-6xl">
+          <h1>Algae  Based</h1>
           <h1>Photo Reactor</h1>
         </div>
       </div>
-      <div className="graphs grid grid-cols-2 w-screen px-5 mt-5 gap-3 align-center min-h-[260px] border-black border-2">
-        <div className="AQI border-2 border-purple-600 text-center w-1/2">
+      
+      <div className="graphs w-full px-10 mt-5 gap-3 h-[40.5rem]">
+        <div className="flex font-semibold justify-between px-8">
+        <div className="AQI text-center h-[15rem] w-[16rem]">
           <Image src={"/image3.jpg"} className="object-cover w-full h-full" width={302} height={256} />
+          <h3>Air Quality Index</h3>
         </div>
         
-        <div className="ratio border-2 border-purple-600 text-center w-1/2">
-          <Image src={"/image4.jpg"} className="object-cover w-[1024px] h-full" width={302} height={256} />
+        <div className="ratio text-center h-[282px] w-[408px]">
+          <Image src={"/img6.png"} className="object-contain w-full h-full" width={1000} height={1000} />
+          <h3>CO2 intake and Output Ratio</h3>
+        </div>
         </div>
 
-        <div className="density border-2 border-purple-600 text-center">
-          <Image src={"/image5.png"} width={452} height={290}/>
+        <div className="density font-semibold border-2 mt-8 mx-auto h-[15.5rem] w-[25.5rem] text-center">
+          <Image src={"/image5.png"} width={452} height={310}/>
+          <h3>Algae Density</h3>
         </div>
       </div>
       <div className="info border-2 my-5 px-3 border-red-600">
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo tenetur beatae laboriosam!</p>
+      
       </div>
     </div>
   </>);
